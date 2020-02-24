7834e62d624b9383e0634052dc5079adc873935d
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-02-24 18:51:04 +0100

    Add the ability to delete a transaction
    This mark the first tag!

--- src/components/TransactionListItem.vue
+++ src/components/TransactionListItem.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="shadow-sm bg-white p-3 rounded mb-3 flex border-r-8" :class="borderColor">
-    <div class="flex-grow">{{ transaction.description }}</div>
-    <Currency :amount="transaction.amount" />
+  <div class="relative">
+    <a class="absolute bg-red-600 text-white px-1 rounded cursor-pointer" style="left: -1rem;" @click.prevent="removeTransaction(transaction.id)">
+      <Icon icon="trash-alt" />
+    </a>
+    <div class="shadow-sm bg-white p-3 rounded mb-3 flex border-r-8" :class="borderColor">
+      <div class="flex-grow">{{ transaction.description }}</div>
+      <Currency :amount="transaction.amount" />
+    </div>
   </div>
 </template>
 
 <script>
 import Currency from './Currency';
+import Icon from './Icon';
+import { mapActions } from 'vuex';
 
 export default {
   name: 'TransactionListItem',
@@ -18,7 +25,9 @@ export default {
     }
   },
 
-  components: { Currency },
+  components: { Icon, Currency },
+
+  methods: mapActions(['removeTransaction']),
 
   computed: {
     borderColor() {
--- src/fonts.js
+++ src/fonts.js
@@ -1,4 +1,4 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEuroSign } from '@fortawesome/free-solid-svg-icons'
+import { faEuroSign, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faEuroSign);
+library.add(faEuroSign, faTrashAlt);
