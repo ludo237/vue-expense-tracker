5fa7593ebda430bea7b9361ad84377bfdf8ace51
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-02-28 10:52:54 +0100

    Fix connection issue with mongodb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 dotenv.config({ path: '.env' });
@@ -10,7 +9,7 @@ const connectDB = async () => {
         const connection = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useCreateIndex: true,
-            useUnifiedTopology: true,
+            useUnifiedTopology: true
         });
 
         console.info(`DB online: ${connection.connection.host}`);
